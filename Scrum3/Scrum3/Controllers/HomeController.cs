217e2ced7db4c8bc26288b299af5cb3f78dbcf79
LasseCa <52892590+LasseCa@users.noreply.github.com>
2019-11-29 19:24:28 +0200

    Model lisätty

--- Scrum3/Scrum3/Controllers/HomeController.cs
+++ Scrum3/Scrum3/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Scrum3.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -23,6 +24,27 @@ namespace Scrum3.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Hae()
+        {
+            var x = "";
+
+            ScrumEntities db = new ScrumEntities();
+            var a = from b in db.Opiskelijat
+                    select b;
+            foreach (var s in a)
+            {
+
+                x += s.Etunimi.ToString() + s.Sukunimi.ToString();
+
+            }
+                ViewBag.Message = x; 
+
+
+
 
             return View();
         }
