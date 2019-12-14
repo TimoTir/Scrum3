2611ba960d44741cf9d04994b1ff2cc776268a45
LasseCa <52892590+LasseCa@users.noreply.github.com>
2019-12-14 14:43:02 +0200

    Opettajan tulostusnäkymä lisätty

---
+++ Scrum3/Scrum3/Controllers/OpettajaController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Scrum3.Model;
+using Scrum3.ViewModels;
+
+namespace Scrum3.Controllers
+{
+    public class OpettajaController : Controller
+    {
+        private ScrumEntities1 db = new ScrumEntities1();
+        
+        // GET: Opettaja
+        public ActionResult Index()
+        { 
+       if ((Session["UserName"] != null) && (Session["AccessLevel"].ToString() == "2"))
+                {
+                var osallistumiset = from os in db.Osallistumiset
+                                     join kt in db.KurssiToteutukset on os.KurssitoteutusID equals kt.KurssitoteutusID
+                                     join ku in db.Kurssit on kt.Kurssi equals ku.KurssiId
+                                     join op in db.Opiskelijat on os.OppilasID equals op.Opiskelijanumero
+                                     select new Class1
+                                     {
+                                         KurssitoteutusID = kt.KurssitoteutusID,
+                                         Kurssi = ku.Kurssi,
+                                         Laajuus = (int)ku.Laajuus,
+                                         Paivamaara = (DateTime)kt.Paivamaara,
+                                         Etunimi = op.Etunimi,
+                                         Sukunimi = op.Sukunimi,
+                                         OsallistumisetID = os.OsallistumisetID
+                                     };
+
+                
+                return View(osallistumiset);
+    } else
+            {  
+                
+                
+                
+                return RedirectToAction("Index", "Home");
+
+            };
+                
+                
+        }
+    }
+
+
+           
+        
+
+       
+
+        
+        
+    
+}
